01e978f11fd48438a2952a47345971ea3e71a669
Leo Ferreira <leonardoferreira@protonmail.com>
2022-10-12 17:48:38 -0300

    tailwind theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import './styles/global.css'
+import "./styles/global.css";
 
 export function App() {
   return (
-    <h1>Hello world</h1>
-  )
+    <>
+      <h1>Hello world</h1>
+      <button className="bg-cyan">button</button>
+    </>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      black: "#000000",
+      cyan: "#61DAFB",
+      "grey-900": "#202024",
+      "grey-800": "#3E3E3E",
+      "grey-600": "#B9B9B9",
+      "grey-400": "#D9D9D9",
+    },
     extend: {
       fontFamily: {
-        sans: 'Inter, sans-serif'
-      }
+        sans: "Inter, sans-serif",
+      },
     },
   },
   plugins: [],
-}
+};
