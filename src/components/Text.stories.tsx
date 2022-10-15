dae6a909c7a267fd1b0c3214d169a23904c28722
Leo Ferreira <leonardoferreira@protonmail.com>
2022-10-14 22:15:10 -0300

    Texr storybook config

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,13 @@
-import { Text } from "./Text";
+import { Meta, StoryObj } from "@storybook/react";
+import { Text, TextProps } from "./Text";
 
 export default {
   title: "Components/text",
   component: Text,
-};
+  args: {
+    size: "md",
+    children: "test",
+  },
+} as Meta<TextProps>;
 
-export const Default = {};
+export const Default: StoryObj = {};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,7 +1,20 @@
-interface TextProps {
+import { clsx } from "clsx";
+
+export interface TextProps {
   size?: "sm" | "md" | "lg";
+  children: string;
 }
 
-export function Text({ size = "md" }: TextProps) {
-  return <h1 className=''>Text</h1>;
+export function Text({ size = "md", children }: TextProps) {
+  return (
+    <h1
+      className={clsx("text-grey-600 font-sans", {
+        "text-sm": size === "sm",
+        "text-md": size === "md",
+        "text-lg": size === "lg",
+      })}
+    >
+      {children}
+    </h1>
+  );
 }
